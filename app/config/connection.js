1710ea1c6fb8a891e07810541e2090b064ab5eca
ideascomealive <jim@ideascomealive.com>
2016-07-25 19:42:36 -0500

    update to jawsDB

--- app/config/connection.js
+++ app/config/connection.js
@@ -4,9 +4,9 @@
 
 var mysql = require('mysql');
 
-// Edit this connection to make it work with JawsDB.
+// we placed the connections in this source object
 var source = {
-
+    // localhost
     localhost: {
         port: 3306,
         host: 'localhost',
@@ -14,19 +14,21 @@ var source = {
         password: "Njmitx123",
         database: "starwars"
     },
-    jawsDB: {
 
-        // TODO: Add your JawsDB connection info here
+    // jawsDB
+    jawsDB: {
+        port: 3306,
+        host: 'bqmayq5x95g1sgr9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'grdzj91e2tip9fus',
+        password: "b6ilubvoezzjb0ng",
+        database: "un66s53fwl4y22lk" 
     }
 }
 
-
-// TODO: connect your jawsDB connection witht he mysql connection.
-var connection = mysql.createConnection( //enter connection here);
-
+// we use source.[name of connection] to hook into mysql
+var connection = mysql.createConnection(source.jawsDB);
 
 
-// Replace the connection code here with one to connect to you JawsDB connection.
 connection.connect(function(err) {
     if (err) {
         console.error('error connecting: ' + err.stack);
